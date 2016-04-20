2d52664c16f234bbe811bbe09827c4923a90be11
John Tate <jt6@sanger.ac.uk>
2016-04-20 11:49:14 +0100

    Add new functionality to delete MIDAS users
    Add method to the User schema to handle deletion, and add a new method,
    "find_user", that takes notice of the "deleted_at" field to correctly
    ignore deleted users.

--- lib/Bio/HICF/User.pm
+++ lib/Bio/HICF/User.pm
@@ -45,6 +45,7 @@ use Try::Tiny;
 use Email::Valid;
 use File::Basename;
 use List::MoreUtils qw( mesh );
+use DateTime;
 
 use Bio::Metadata::Types qw( UUID OntologyTerm );
 use Bio::Metadata::Checklist;
@@ -132,6 +133,61 @@ sub add_new_user {
 
 #-------------------------------------------------------------------------------
 
+=head2 find_user($username)
+
+Returns the row for the specified user. Requires one argument, giving the
+username of the user. Croaks if the username is not supplied. Returns undef if
+the username does not return a row.
+
+=cut
+
+sub find_user {
+  my ( $self, $username ) = @_;
+
+  croak 'ERROR: must supply a username'
+    unless defined $username;
+
+  my $user_rs = $self->resultset('User')->search(
+    {
+      username   => $username,
+      deleted_at => undef,
+    }
+  );
+
+  return unless ( defined $user_rs and $user_rs->count == 1 );
+
+  return $user_rs->first;
+}
+
+#-------------------------------------------------------------------------------
+
+=head2 delete_user($username)
+
+Delete the specified user. Requires one argument, the username of the user to
+be deleted. Croaks is a username is not supplied, or if a user with that
+username is not found.
+
+Note that the user account is not really deleted. Instead, its "deleted_at"
+field is set to the current time.
+
+=cut
+
+sub delete_user {
+  my ( $self, $username ) = @_;
+
+  croak 'ERROR: must supply a username'
+    unless defined $username;
+
+  my $user = $self->find_user($username);
+
+  croak "ERROR: user '$username' does not exist"
+    unless defined $user;
+
+  $user->update( { deleted_at => DateTime->now } );
+}
+
+#-------------------------------------------------------------------------------
+
 =head2 update_user($user_details)
 
 Update the details for the specified user. Requires a single argument, a
@@ -182,8 +238,7 @@ sub update_user {
 
   # TODO maybe implement roles
 
-  my $user = $self->resultset('User')
-                  ->find( $column_values->{username} );
+  my $user = $self->find_user( $column_values->{username} );
 
   croak "ERROR: user '$fields->{username}' does not exist; use 'add_user' to add"
     unless defined $user;
@@ -205,8 +260,7 @@ sub set_passphrase {
   croak 'ERROR: must supply a username and a passphrase'
     unless ( defined $username and defined $passphrase );
 
-  my $user = $self->resultset('User')
-                  ->find($username);
+  my $user = $self->find_user($username);
 
   croak "ERROR: user '$username' does not exist; use 'add_user' to add"
     unless defined $user;
@@ -228,7 +282,7 @@ sub reset_passphrase {
 
   croak 'ERROR: must supply a username' unless defined $username;
 
-  my $user = $self->resultset('User')->find($username);
+  my $user = $self->find_user($username);
 
   croak "ERROR: user '$username' does not exist; use 'add_user' to add"
     unless defined $user;
@@ -250,8 +304,7 @@ sub reset_api_key {
 
   croak 'ERROR: must supply a username' unless defined $username;
 
-  my $user = $self->resultset('User')
-                  ->find($username);
+  my $user = $self->find_user($username);
 
   croak "ERROR: user '$username' does not exist; use 'add_user' to add"
     unless defined $user;
--- t/09_password_methods.t
+++ t/09_password_methods.t
@@ -2,9 +2,8 @@
 use strict;
 use warnings;
 
-use Test::More tests => 40;
+use Test::More tests => 49;
 use Test::Exception;
-use Test::Script::Run;
 
 #-------------------------------------------------------------------------------
 # DB setup
@@ -41,8 +40,44 @@ lives_ok { Schema->add_new_user($user_details) }
 
 is( User->count, 1, 'one user after load' );
 
+# "find_user"
+is Schema->find_user('nonexistentuser'), undef,
+  '"find_user" returns undef for non-existent user';
+
+throws_ok { Schema->find_user }
+  qr/must supply a username/,
+  'exception from "find_user" with no username';
+
 my $user;
-ok( $user = User->find('user1'), 'got row using username' );
+ok $user = Schema->find_user('user1'), '"find_user" returns a user';
+
+isa_ok $user, 'Bio::HICF::User::Result::User', 'got User row';
+
+my $user_row = User->find('user1');
+is $user_row->deleted_at, undef, '"deleted_at" has NO value for live user';
+
+# "delete_user"
+throws_ok { Schema->delete_user }
+  qr/must supply a username/,
+  'exception from "delete_user" with no username';
+
+throws_ok { Schema->delete_user('nonexistentuser') }
+  qr/does not exist/,
+  'exception from "delete_user" with non-existent username';
+
+# delete a real user and make sure we don't get it back with "find_user"
+lives_ok { Schema->delete_user('user1') }
+  'no exception from "delete_user" with valid username';
+
+is Schema->find_user('user1'), undef, 'no deleted user via "find_user"';
+
+$user_row = User->find('user1');
+ok $user_row->deleted_at, '"deleted_at" has a value for deleted user';
+
+# re-enable the deleted user
+$user_row->update( { deleted_at => undef } );
+
+# check other stored values for the user
 is( $user->email, $user_details->{email}, 'got expected email address from DB' );
 
 ok( $user->check_password('user1_passphrase'), 'passphrase checks out' );
@@ -52,10 +87,12 @@ isa_ok( $user->passphrase, 'Authen::Passphrase' );
 ok( $user->passphrase('new_passphrase'), 'successfully reset passphrase for user' );
 ok( $user->check_password('new_passphrase'), 'new passphrase checks out' );
 
+# can't create two users with the same username
 throws_ok { Schema->add_new_user($user_details) }
   qr/user already exists/,
   'got expected error when adding same user again';
 
+# add a second user
 $user_details->{username} = 'user2';
 
 my $returned_passphrase;
@@ -143,5 +180,5 @@ throws_ok { Schema->reset_api_key('nonexistentuser') }
 lives_ok { $key = Schema->reset_api_key('user1') } 'can reset password via schema';
 isnt( $key, $new_key, 'key has changed' );
 
-done_testing;
+# done_testing;
 
